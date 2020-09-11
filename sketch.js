9b37a29777b56a41f405dbb284d7e682035dc620
Rohit1111-BEST <65074708+Rohit1111-BEST@users.noreply.github.com>
2020-09-11 13:01:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,85 +1,68 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground, box1,box2,box3;
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-
-var engine;
-var world;
-
-function preload()
-{
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
-}
+var wall, thickness;
+var bullet,speed, weight;
 
 function setup() {
-	createCanvas(800, 700);
-	rectMode(CENTER);
-	
-	engine = Engine.create();
-	world = engine.world;
-	
-	packageBody= new Package(width/2, 80,10);		
+  createCanvas(1600,400);
+speed=random(223,321);
+weight=random(30,52);
+bullet=createSprite(50,200,50,50);
+wall=createSprite(1200,200,thickness,height/2);
+wall.shapeColor=(80,80,80);
 
 
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 80, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	//packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
-	//World.add(world, packageBody);
-
-	ground=new Ground(width/2, height-35, width,10);
-box1 = new Box(420,height-50, 70,10);
-box2 = new Box(460,height-80,10,50);
-box3 = new Box(380,height-80,10,50);
-	
-
-	//packageBody=new Package(100,700,20);
-
-
-
-	//Create a Ground
-	
-
-	Engine.run(engine);
-  
 }
 
+
+function hasCollided(lbullet, lwall)
+{
+bulletRightEdge=lbullet.x +lbullet.width;
+wallLeftEdge=lwall.x;
+if (bulletRightEdge>=wallLeftEdge){
+  return true
+}
+return false;
+}
+
+if (hasCollided(bullet,wall))
+{
+  bullet.velocityX=0;
+  var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+}
+
+  if(damage>10)
+  {
+    wall.shapeColor=color(255,0,0)
+}
+
+if(damage<10)
+{
+  wall.shapeColor=color(0,255,0)
+}
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
+  background(80,80,80);
+ /* bullet.velocityX = speed;
+
+  thickness=random(23,83);
+
+if(wall.x-bullet.x < (bullet.width+wall.width)/2){
   
+  bullet.velocityX=0;
+var deformation=0.5 * weight * speed* speed/22509;
+if(deformation>180)
+{
+  bullet.shapeColor=color(255,0,0);
+}
+if(deformation<180 && deformation>100)
+{
+  bullet.shapeColor=color(230,230,0);
+}
+if(deformation<100)
+{
+  bullet.shapeColor=color(0,255,0)
+}
 
+}*/
 
- packageSprite.x = packageBody.body.position.x;
- packageSprite.y= packageBody.body.position.y;
-
-  packageBody.display();
-  ground.display();
-  box1.display();
-  box2.display();
-  box3.display();
   drawSprites();
-
-
 }
-
-function keyPressed() {
-
- if (keyCode === DOWN_ARROW) {
-	
-    // Look at the hints in the document and understand how to make the package body fall only on
-    Matter.Body.setStatic(packageBody,false);
-  }
-}
-
-
-
