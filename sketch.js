a97d669e9a40e47da0cedd76e37507cd0b8e1fe9
Rohit1111-BEST <65074708+Rohit1111-BEST@users.noreply.github.com>
2020-09-12 05:38:37 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -23,22 +23,8 @@ if (bulletRightEdge>=wallLeftEdge){
 return false;
 }
 
-if (hasCollided(bullet,wall))
-{
-  bullet.velocityX=0;
-  var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
 
 
-  if(damage>10)
-  {
-    wall.shapeColor=color(255,0,0)
-}
-
-if(damage<10)
-{
-  wall.shapeColor=color(0,255,0)
-}
-}
 
 function draw() {
   background(80,80,80);
@@ -64,6 +50,23 @@ if(deformation<100)
 }
 
 }*/
+
+if (hasCollided(bullet,wall))
+{
+  bullet.velocityX=0;
+  var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+
+
+  if(damage>10)
+  {
+    wall.shapeColor=color(255,0,0)
+}
+
+if(damage<10)
+{
+  wall.shapeColor=color(0,255,0)
+}
+}
 
   drawSprites();
 }
